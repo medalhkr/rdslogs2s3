96bc95c5ebade4d34a8f47d18851f7d8bcc405b8
Hikaru Inomoto <hikaru.inomoto@jp.oro.com>
2018-01-24 20:38:11 +0900

    add timestamp file

--- rdslogs2s3.py
+++ rdslogs2s3.py
@@ -1,21 +1,12 @@
 #!/bin/env python
 # coding=utf-8
 #
-# environment
-# ```
-# export RDS_INSTANCE=<rds instance name>
-# export LOG_NAME=<rds log name>
-# export S3_BUCKET=<save log bucker name>
-# export S3_KEY_PREFIX=<save key prefix>
-# export REGION=<aws region>
-# export TZ=<timezone>
-# ```
-#
 import sys
 import os
 import gzip
-import boto3
 from datetime import datetime
+import time
+import boto3
 from botocore.exceptions import ClientError
 
 REGION = os.environ['REGION']
@@ -27,7 +18,6 @@ def lambda_handler(event, context):
         os.environ['RDS_INSTANCE'],
         os.environ['LOG_NAME'],
         os.environ['S3_BUCKET'],
-        os.environ['S3_KEY_PREFIX'],
     )
 
 def copy_log(instance, log_file_name, s3_bucket, s3_prefix):
@@ -58,18 +48,37 @@ def copy_log(instance, log_file_name, s3_bucket, s3_prefix):
 
     return True
 
-def rdslogs2s3(RDS_INSTANCE, LOG_NAME, S3_BUCKET):
-    now = datetime.now().strftime('%Y%m%d%H%M%S')
+def fetch_updated_at(s3_bucket, filename):
+    try:
+        obj = s3_client.get_object(Bucket=s3_bucket, Key=filename)
+        return str(object=obj['Body'].read(), encoding='utf-8')
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'NoSuchKey':
+            return '0'
+        raise e
 
-    db_logs = rds_client.describe_db_log_files(DBInstanceIdentifier=RDS_INSTANCE, FilenameContains=LOG_NAME)
+def rdslogs2s3(RDS_INSTANCE, LOG_NAME, S3_BUCKET):
+    s3_prefix = 'db{0}/{1}/'.format(RDS_INSTANCE, LOG_NAME)
+    timestamp_filename = s3_prefix + 'updated_at'
+
+    try:
+        db_logs = rds_client.describe_db_log_files(
+            DBInstanceIdentifier=RDS_INSTANCE,
+            FilenameContains=LOG_NAME,
+            FileLastWritten=fetch_updated_at(S3_BUCKET, timestamp_filename)
+        )
+    except ClientError as e:
+        print("Unexpected error: {}".format(e))
+        return False
 
     for db_log in db_logs['DescribeDBLogFiles']:
         log_file_name = db_log['LogFileName']
-
-        s3_prefix = 'db{0}/{1}/'.format(RDS_INSTANCE, LOG_NAME)
         copy_log(RDS_INSTANCE, log_file_name, S3_BUCKET, s3_prefix)
 
-    return "Success"
+    checked_timestamp = int(time.mktime(datetime.now().timetuple())) * 1000
+    s3_client.put_object(Bucket=S3_BUCKET, Key=timestamp_filename, Body=str(checked_timestamp))
+
+    return True
 
 if __name__ == "__main__":
     lambda_handler(None, None)
