e05034fd588334fc71c4d898f02b8652f2ca69d7
Hikaru Inomoto <hikaru.inomoto@jp.oro.com>
2018-02-02 18:00:25 +0900

    change last-update-timestamp to save
    if `describe_db_log_files` miss some file, these files skipped next batch.
    
    for instance:
    batch at 15:56 -> find log-at-14:30 and miss log-at-15:30. and last_updated_at = 15:56.
    batch at 16:56 -> search from 15:56, and no logs found.
    batch at 17:56 -> search from 15:56, and find log-at-16:30 and miss log-at-15:30.

--- rdslogs2s3.py
+++ rdslogs2s3.py
@@ -57,26 +57,24 @@ def fetch_updated_at(s3_bucket, filename):
 def rdslogs2s3(rds_instance, log_name, s3_bucket):
     s3_prefix = 'db_{0}/'.format(rds_instance)
     timestamp_filename = '{0}{1}.updated_at'.format(s3_prefix, log_name)
+    last_checked = fetch_updated_at(s3_bucket, timestamp_filename)
 
     try:
         db_logs = RDS_CLIENT.describe_db_log_files(
             DBInstanceIdentifier=rds_instance,
             FilenameContains=log_name + '.', # specify rotated log file
-            FileLastWritten=fetch_updated_at(s3_bucket, timestamp_filename)
+            FileLastWritten=last_checked
         )
     except ClientError as e:
         print("Unexpected error: {}".format(e))
         return False
 
-    if len(db_logs['DescribeDBLogFiles']) == 0:
-        return True # skip update checked_timestamp if there is no file to upload
-
     for db_log in db_logs['DescribeDBLogFiles']:
         log_file_name = db_log['LogFileName']
+        last_checked = db_log['LastWritten']
         copy_log(rds_instance, log_file_name, s3_bucket, s3_prefix)
 
-    checked_timestamp = int(time.mktime(datetime.now().timetuple())) * 1000
-    S3_CLIENT.put_object(Bucket=s3_bucket, Key=timestamp_filename, Body=str(checked_timestamp))
+    S3_CLIENT.put_object(Bucket=s3_bucket, Key=timestamp_filename, Body=last_checked)
 
     return True
 
